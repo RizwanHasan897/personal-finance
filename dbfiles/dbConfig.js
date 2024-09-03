0c83a3c7cce9a026f8cc08750c07c4c32337345a
Rizwan Hasan <rizwanhasan897@gmail.com>
2024-09-03 20:28:01 +0100

    Update dbConfig.js

--- dbfiles/dbConfig.js
+++ dbfiles/dbConfig.js
@@ -1,13 +1,13 @@
 const config ={
-    user: "fiadmin",
-    password: "foo",
-    server: "DESKTOP-R2OSPD2",
-    database: "perfisql",
+    user: "uk6dmk1h2bklszld",
+    password: "XaZZd2yNp6l8bgRwUCyR",
+    server: "mysql://uk6dmk1h2bklszld:XaZZd2yNp6l8bgRwUCyR@b2iovoiqthamybvnuagp-mysql.services.clever-cloud.com:3306/b2iovoiqthamybvnuagp",
+    database: "b2iovoiqthamybvnuagp",
     option: {
         encrypt: true,
         trustServerCertificate: true
     },
-    Port: 1433
+    Port: 3306
 }
 
 export default config;
